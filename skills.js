60a7d13dde311dd06314afd59721f86266aefa45
radziobwlkp <nowakr1988@gmail.com>
2025-02-05 18:08:56 +0000

    Dodano funkcję calculateNumbers

---
+++ skills.js
@@ -0,0 +1,8 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+// Test funkcji
+console.log(calculateNumbers(3, 4)); // Oczekiwany wynik: 7
+console.log(calculateNumbers(-1, 5)); // Oczekiwany wynik: 4
+console.log(calculateNumbers(0, 0)); // Oczekiwany wynik: 0
